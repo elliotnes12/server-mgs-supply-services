3bcc9ad6e8c8463c7dda5123fb56e61740a572f2
Vantis Elliot <nem.elliot@coppel.com>
2024-09-17 18:38:45 -0600

    activando usuario , modificando servicio

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ import { mdAuth } from "../middlewares/index.js";
 
 const api = express.Router();
 
-api.post("/auth/generate_code",[mdAuth.asureAuth],authController.generateCode);
+api.post("/auth/generate_code", authController.generateCode);
 
 api.post("/auth/register",authController.registerUser);
 
@@ -12,7 +12,7 @@ api.post("/auth/login",authController.login);
 
 api.post("/auth/refresh_access_token",authController.refreshToken);
 
-api.post("/auth/validate_code",[mdAuth.asureAuth],authController.activateUser);
+api.post("/auth/validate_code", authController.activateUser);
 
 
 export const authRoutes = api;
